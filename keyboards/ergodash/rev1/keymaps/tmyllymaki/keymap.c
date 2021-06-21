c68cd20cf6b7c60db04faf32d01de6fd3e9175ff
tmyllymaki <timo.myllymaki1@gmail.com>
2021-06-21 08:02:27 +0300

    lambda arrow macro

--- keyboards/ergodash/rev1/keymaps/tmyllymaki/keymap.c
+++ keyboards/ergodash/rev1/keymaps/tmyllymaki/keymap.c
@@ -1,5 +1,6 @@
 #include QMK_KEYBOARD_H
 #include "keymap_finnish.h"
+#include "sendstring_finnish.h"
 
 
 #define _QWERTY 0
@@ -12,6 +13,7 @@ enum custom_keycodes {
   LOWER,
   RAISE,
   ADJUST,
+  LAMBDA_ARROW
 };
 
 #define EISU LALT(KC_GRV)
@@ -55,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT(
     KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LCBR,                        KC_RCBR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, LAMBDA_ARROW, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
     KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_DEL ,                        KC_BSPC, FI_TILD, FI_LCBR,    FI_RCBR,    FI_LBRC,    FI_RBRC,     FI_PIPE , \
     KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_SPC ,                        KC_ENT , FI_LABK,    FI_RABK,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT, \
     KC_LCTL, KC_LGUI, KC_LALT, KC_RALT,             LOWER,   KC_SPC ,KC_DEL,         KC_BSPC,KC_ENT , RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
@@ -150,6 +152,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-  }
+    case LAMBDA_ARROW:
+        if (record->event.pressed) {
+            SEND_STRING("=>");
+        } else {
+        }
+        break;
+    }
   return true;
 }
